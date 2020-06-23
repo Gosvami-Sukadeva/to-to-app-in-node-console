e2131620cdf1cc48693c8ed21e9dddc304606ac3
Mateusz Pilarczyk <bhakta666@gmail.com>
2020-06-23 11:31:28 +0200

    update

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 const parseArgs = require("minimist");
 const colors = require("colors");
+const fs = require("fs");
 const command = parseArgs(process.argv.slice(2, 3));
 delete command._;
 // console.log(command);
@@ -14,14 +15,44 @@ const handleCommand = ({ add, remove, list }) => {
     } else if (add.length < 7) {
       return console.log("nazwa zadania musi mieć więcej niż 6 znaków".red);
     }
-    console.log("będę dodawać coś");
+
+    handleData(1, add);
   } else if (remove) {
-    console.log("będę coś usuwać");
-  } else if (list) {
-    console.log("pokazuję listę");
+    if (typeof remove !== "string" || remove.length < 7) {
+      return console.log(
+        "wpisz nazwę usuwanego zadania. To musi być tekst i musi mieć więcej niż 6 znaków"
+          .red
+      );
+    }
+    handleData(2, remove);
+  } else if (list || list === "") {
+    handleData(3, null);
   } else {
-    console.log("Nie rozumiem polecenia");
+    console.log(
+      'Nie rozumiem polecenia. Użyj --add="nazwa zadania",--remove="nazwa zadania" lub opcji --list'
+    );
   }
 };
 
+const handleData = (type, title) => {
+  //type - number (1 - add; 2 - remove; 3 - list)
+  // title(string || null)
+
+  let data = fs.readFileSync("data.json");
+  // const data = fs.readFileSync("data.json", "utf8");
+  // let data = fs.readFileSync("data.json");
+  //
+  // data = data.toString();
+  const tasks = JSON.parse(data);
+  console.log(tasks);
+
+  if (type === 1 || type === 2) {
+    isExisted = tasks.find((task) => task.title === title) ? true : false;
+    if (type === 1 && isExisted) {
+      console.log("takie zadanie już istnieje");
+    } else if (type === 2 && !isExisted) {
+      return console.log("Nie mogę usunąć zadania, które nie istnieje");
+    }
+  }
+};
 handleCommand(command);
