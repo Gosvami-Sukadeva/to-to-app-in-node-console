8754a653b74d0d0c4af72b74c68b7309f26a1c47
Mateusz Pilarczyk <bhakta666@gmail.com>
2020-06-24 10:45:18 +0200

    add remove task method

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ const handleCommand = ({ add, remove, list }) => {
   } else {
     console.log(
       'Nie rozumiem polecenia. Użyj --add="nazwa zadania",--remove="nazwa zadania" lub opcji --list'
+        .red
     );
   }
 };
@@ -49,29 +50,45 @@ const handleData = (type, title) => {
   if (type === 1 || type === 2) {
     isExisted = tasks.find((task) => task.title === title) ? true : false;
     if (type === 1 && isExisted) {
-      return console.log("takie zadanie już istnieje");
+      return console.log("takie zadanie już istnieje".red);
     } else if (type === 2 && !isExisted) {
-      return console.log("Nie mogę usunąć zadania, które nie istnieje");
+      return console.log("Nie mogę usunąć zadania, które nie istnieje".red);
     }
   }
+  let dataJSON = "";
   switch (type) {
     case 1:
       console.log("dodaje zadanie");
       const id = tasks.length + 1;
       tasks.push({ id, title });
       console.log(tasks);
-      const dataJSON = JSON.stringify(tasks);
+      dataJSON = JSON.stringify(tasks);
       console.log(dataJSON);
       fs.writeFileSync("data.json", dataJSON);
       console.log(`dodaje zadanie: ${title}`.white.bgGreen);
       break;
 
     case 2:
-      console.log("usuwam zadanie");
+      const index = tasks.findIndex((task) => task.title === title);
+      tasks.splice(index, 1);
+      console.log(tasks);
+      dataJSON = JSON.stringify(tasks);
+      fs.writeFile("data.json", dataJSON, "utf8", (err) => {
+        if (err) throw err;
+        console.log(`Zadanie ${title} zostało usunięte`.white.bgGreen);
+      });
       break;
 
     case 3:
-      console.log("wyświetlam listę");
+      console.log(
+        `Lista zadań do zrobienia obejmuje ${tasks.length} pozycji. Do zrobienia masz: `
+      );
+      if (tasks.length) {
+        tasks.forEach((task, index) => {
+          if (index % 2) return console.log(task.title.green);
+          return console.log(task.title.yellow);
+        });
+      }
       break;
   }
 };
